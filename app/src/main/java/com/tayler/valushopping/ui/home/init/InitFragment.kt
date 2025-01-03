33046b4deb8f428b8ae4701361a7395034dad9d4
Tayler <emontesinos812@gmail.com>
2025-01-03 12:42:47 -0500

    config movie

--- app/src/main/java/com/tayler/valushopping/ui/home/init/InitFragment.kt
+++ app/src/main/java/com/tayler/valushopping/ui/home/init/InitFragment.kt
@@ -4,15 +4,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
 import com.tayler.valushopping.R
 import com.tayler.valushopping.databinding.FragmentInitBinding
+import com.tayler.valushopping.entity.singleton.AppDataVale
 import com.tayler.valushopping.ui.BaseFragment
+import com.tayler.valushopping.utils.EMPTY_VALE
 
 
 class InitFragment : BaseFragment() {
 
     private lateinit var binding: FragmentInitBinding
-
     companion object { fun newInstance() = InitFragment() }
 
     override fun getMainView(inflater: LayoutInflater, container: ViewGroup?): View {
@@ -24,10 +28,19 @@ class InitFragment : BaseFragment() {
     }
 
     override fun setUpView() {
+        val param = AppDataVale.paramData
+        binding.textTitleInit.text = param.title
+        binding.textSubTitleInit.text = param.description
+        val youTubePlayerView: YouTubePlayerView = binding.youTubeVale
+        lifecycle.addObserver(youTubePlayerView)
+        youTubePlayerView.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
+            override fun onReady(youTubePlayer: YouTubePlayer) {
+                val videoId = param.idMovie?: EMPTY_VALE
+                youTubePlayer.loadVideo(videoId, 0f)
+            }
+        })
     }
 
     override fun observeLiveData() {
     }
-
-
 }
